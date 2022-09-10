80776a2ea5ddd5f16b8b0c52be7da8f2408a0589
rakim-up <ruraleproctor@gmail.com>
2022-09-10 14:09:52 +1200

    Nav made, just need the useNavigate and pictures working

--- client/components/Nav/Nav.jsx
+++ client/components/Nav/Nav.jsx
@@ -6,7 +6,30 @@ import { useNavigate } from "react-router-dom";
 const Nav = () => {
   
 return (
-  <h1 className="text-2xl"> NAV BAR HERE  </h1>
+<>
+  <nav className="p-5 bg-white shadow md:flex md:items-center md:justify-between">
+    <div>
+      <span className="text-2xl font-[Poppins]">
+        Pusi Urale
+      </span>
+     </div>
+
+    <ul className="md:flex md:items-center z-[-1] md:z-auto ">
+      <li className="mx-4 my-6 md:my-0">
+        <button className="text-xl hover:text-cyan-500 duration-500">ABOUT</button>
+      </li>
+      <li className="mx-4 my-6 md:my-0">
+        <button className="text-xl hover:text-cyan-500 duration-500">EXHIBITIONS</button>
+      </li>
+      <li className="mx-4 my-6 md:my-0">
+        <button className="text-xl hover:text-cyan-500 duration-500">ARTICLES</button>
+      </li>
+      <li className="mx-4 my-6 md:my-0">  
+        <button className="text-xl hover:text-cyan-500 duration-500">FOR SALE</button>
+      </li>
+    </ul>
+  </nav>
+</>
 )
 }
 
